6b489cd2313fd637f2b9230925e5f9807a44b220
Artem Vetrov <a.vetrov@bankingon.io>
2022-10-23 16:58:29 +0200

    Прописаны кнопки к цифрам и основным действиям

--- Calculator/Calculator/ViewModels/MainWindowViewModel.cs
+++ Calculator/Calculator/ViewModels/MainWindowViewModel.cs
@@ -25,6 +25,8 @@ namespace Calculator.ViewModels
             }
         }
 
+        #region Кнопки
+
         /// <summary>
         /// Нажатие на кнопку 7
         /// </summary>
@@ -40,11 +42,72 @@ namespace Calculator.ViewModels
         /// </summary>
         public ReactiveCommand<Unit, Unit> PressNineCommand { get; }
 
-
         /// <summary>
         /// Нажатие на кнопку +
         /// </summary>
         public ReactiveCommand<Unit, Unit> PressPlusCommand { get; }
+
+        /// <summary>
+        /// Нажатие на кнопку 4
+        /// </summary>
+        public ReactiveCommand<Unit, Unit> PressFourCommand { get; }
+
+        /// <summary>
+        /// Нажатие на кнопку 5
+        /// </summary>
+        public ReactiveCommand<Unit, Unit> PressFiveCommand { get; }
+
+        /// <summary>
+        /// Нажатие на кнопку 6
+        /// </summary>
+        public ReactiveCommand<Unit, Unit> PressSixCommand { get; }
+
+        /// <summary>
+        /// Нажатие на кнопку -
+        /// </summary>
+        public ReactiveCommand<Unit, Unit> PressMinusCommand { get; }
+
+        /// <summary>
+        /// Нажатие на кнопку 1
+        /// </summary>
+        public ReactiveCommand<Unit, Unit> PressOneCommand { get; }
+
+        /// <summary>
+        /// Нажатие на кнопку 2
+        /// </summary>
+        public ReactiveCommand<Unit, Unit> PressTwoCommand { get; }
+
+        /// <summary>
+        /// Нажатие на кнопку 3
+        /// </summary>
+        public ReactiveCommand<Unit, Unit> PressThreeCommand { get; }
+
+        /// <summary>
+        /// Нажатие на кнопку *
+        /// </summary>
+        public ReactiveCommand<Unit, Unit> PressMultiplicationCommand { get; }
+
+        /// <summary>
+        /// Нажатие на кнопку 0
+        /// </summary>
+        public ReactiveCommand<Unit, Unit> PressZeroCommand { get; }
+
+        /// <summary>
+        /// Нажатие на кнопку точки
+        /// </summary>
+        public ReactiveCommand<Unit, Unit> PressPointCommand { get; }
+
+        /// <summary>
+        /// Нажатие на кнопку процента
+        /// </summary>
+        public ReactiveCommand<Unit, Unit> PressChangeSignCommand { get; }
+
+        /// <summary>
+        /// Нажатие на кнопку деления
+        /// </summary>
+        public ReactiveCommand<Unit, Unit> PressDivinationCommand { get; }
+
+        #endregion
 
         /// <summary>
         /// Конструктор, связывающий кнопку с нажатем
@@ -57,6 +120,21 @@ namespace Calculator.ViewModels
             PressEightCommand = ReactiveCommand.Create(OnEightPressed);
             PressNineCommand = ReactiveCommand.Create(OnNinePressed);
             PressPlusCommand = ReactiveCommand.Create(OnPlusPressed);
+
+            PressFourCommand = ReactiveCommand.Create(OnFourPressed);
+            PressFiveCommand = ReactiveCommand.Create(OnFivePressed);
+            PressSixCommand = ReactiveCommand.Create(OnSixPressed);
+            PressMinusCommand = ReactiveCommand.Create(OnMinusPressed);
+
+            PressOneCommand = ReactiveCommand.Create(OnOnePressed);
+            PressTwoCommand = ReactiveCommand.Create(OnTwoPressed);
+            PressThreeCommand = ReactiveCommand.Create(OnThreePressed);
+            PressMultiplicationCommand = ReactiveCommand.Create(OnMultiplicationPressed);
+
+            PressZeroCommand = ReactiveCommand.Create(OnZeroPressed);
+            PressPointCommand = ReactiveCommand.Create(OnPointPressed);
+            PressChangeSignCommand = ReactiveCommand.Create(OnChangeSignPressed);
+            PressDivinationCommand = ReactiveCommand.Create(OnDivinationPressed);
         }
 
         /// <summary>
@@ -66,7 +144,6 @@ namespace Calculator.ViewModels
         {
             OutputText += "7";
         }
-
 
         /// <summary>
         /// Кнопка 8
@@ -84,13 +161,108 @@ namespace Calculator.ViewModels
             OutputText += "9";
         }
 
-
         /// <summary>
         /// Кнопка плюса
         /// </summary>
         private void OnPlusPressed()
         {
             OutputText += "+";
+        }
+
+        /// <summary>
+        /// Кнопка 4
+        /// </summary>
+        private void OnFourPressed()
+        {
+            OutputText += "4";
+        }
+
+        /// <summary>
+        /// Кнопка 5
+        /// </summary>
+        private void OnFivePressed()
+        {
+            OutputText += "5";
+        }
+
+        /// <summary>
+        /// Кнопка 6
+        /// </summary>
+        private void OnSixPressed()
+        {
+            OutputText += "6";
+        }
+
+        /// <summary>
+        /// Кнопка минуса
+        /// </summary>
+        private void OnMinusPressed()
+        {
+            OutputText += "-";
+        }
+
+        /// <summary>
+        /// Кнопка 1
+        /// </summary>
+        private void OnOnePressed()
+        {
+            OutputText += "1";
+        }
+
+        /// <summary>
+        /// Кнопка 2
+        /// </summary>
+        private void OnTwoPressed()
+        {
+            OutputText += "2";
+        }
+
+        /// <summary>
+        /// Кнопка 3
+        /// </summary>
+        private void OnThreePressed()
+        {
+            OutputText += "3";
+        }
+
+        /// <summary>
+        /// Кнопка умножения
+        /// </summary>
+        private void OnMultiplicationPressed()
+        {
+            OutputText += "*";
+        }
+
+        /// <summary>
+        /// Кнопка 0
+        /// </summary>
+        private void OnZeroPressed()
+        {
+            OutputText += "0";
+        }
+
+        /// <summary>
+        /// Кнопка точки
+        /// </summary>
+        private void OnPointPressed()
+        {
+            OutputText += ".";
+        }
+
+        /// <summary>
+        /// Кнопка 3
+        /// </summary>
+        private void OnChangeSignPressed()
+        {
+            OutputText += "±";
+        }
+
+        /// <summary>
+        /// Кнопка 3
+        /// </summary>
+        private void OnDivinationPressed()
+        {
+            OutputText += "/";
         }
     }
 }
